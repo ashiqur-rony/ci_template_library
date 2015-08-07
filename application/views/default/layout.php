c8a505d8e282c1610af429f6667e911537444aa7
GoodKoding <info@goodkoding.com>
2015-08-07 14:35:46 +0600

    Modified the sample layout.

--- application/views/default/layout.php
+++ application/views/default/layout.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-    <title><?php echo (isset($data['title'])? $data['title'] : "My Website Title" ?></title>
+    <title><?php echo (isset($data['title'])? $data['title'] : "My Website Title") ?></title>
 </head>
 <body>
     <div class="wrapper">
